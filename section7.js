262721ab20f0f6eed650755c9a6e1e349669f3d2
alitariq2072008-netizen <alitariq2072008@gmail.com>
2026-01-15 18:35:32 +0300

    Add files via upload

---
+++ section7.js
@@ -0,0 +1,68 @@
+let aliAgo=17;
+if(aliAgo >=18){
+    console.log("Ali can vote ");
+    console.log("Ali cannot vote he is only 17")
+    
+
+
+
+
+}
+
+ 
+
+
+ 
+ let isLoggedIn=true
+ let userName='Ali'
+ 
+ console.log(`Welcome ${userName}`)
+ console.log("Please log in first")
+ 
+ if(isLoggedIn===true) {
+
+console.log('Please log in first')
+
+
+ }
+ let score=76
+ if(score>=90 ){
+
+console.log("Excellent")
+ } if(75>= 89 ){
+    console.log("Very Good")
+    console.log("Good")
+
+
+ }
+ let userAccess="Manager"
+ if(userAccess="Manager"){
+    console.log('Full Access')
+    console.log('Limited Access')
+ }
+
+
+ console.log("task7 done");
+
+ for (let y = 1; y<= 30; y++ ){
+   console.log(y);
+ }
+   
+ 
+
+
+
+
+
+ 
+ 
+
+
+ 
+ 
+  
+
+
+
+
+
